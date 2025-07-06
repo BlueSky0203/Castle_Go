2f231f3649386210d38878aff520c2a1e7aa0329
kevinchung <kevinchung@nefer.com.tw>
2025-07-06 22:06:24 +0800

    chore: support local file and env variable for Firebase admin SDK

--- utils/firebase.go
+++ utils/firebase.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"context"
 	"log"
+	"os"
 
 	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/auth"
@@ -12,7 +13,18 @@ import (
 var FirebaseAuth *auth.Client
 
 func InitFirebase() {
-	opt := option.WithCredentialsFile("firebase-admin.json") // 你的 firebase admin json 路徑
+	// 先嘗試從環境變數讀取 JSON 字串
+	jsonCredentials := os.Getenv("FIREBASE_ADMIN_JSON")
+
+	var opt option.ClientOption
+	if jsonCredentials != "" {
+		// 環境變數存在，使用它初始化
+		opt = option.WithCredentialsJSON([]byte(jsonCredentials))
+	} else {
+		// 環境變數不存在，改用本地檔案（給本機開發用）
+		opt = option.WithCredentialsFile("firebase-admin.json")
+	}
+
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		log.Fatalf("Firebase 初始化失敗: %v", err)
